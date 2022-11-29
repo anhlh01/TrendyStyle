9c34b580761f26f56832ba738e3a21133059b3ad
learning2html <honganhle0a@gmail.com>
2022-11-30 01:44:42 +0700

    [AnhLH] update home page

---
+++ lib/widgets/shop_logo_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/widgets.dart';
+import 'package:trendy_style/utilities/colors.dart';
+
+class ShopLogo extends StatelessWidget {
+  ShopLogo({super.key, required this.icon, required this.name});
+  IconData icon;
+  String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: 65,
+          width: 65,
+          decoration: BoxDecoration(
+            color: AppColors.grayColor,
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Icon(icon),
+        ),
+        Text(name),
+      ],
+    );
+  }
+}
